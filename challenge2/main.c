30aa5180ae5e34c06adf1d991bcec12b1b6115b2
hamza harrass <hamzaharrass05@gamil.com>
2022-09-07 15:16:17 +0100

    challenge2

---
+++ challenge2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float F;
+    float C;
+
+
+
+    printf(" entre la temperature en fahrenh : ");
+    scanf("%f",&F);
+    C=(F-32)/1.8;
+    printf("C=%.2f",C);
+
+    return 0;
+}
