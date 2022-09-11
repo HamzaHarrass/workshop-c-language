a5f57ee2fd0d3f355133e54e47864bb040265843
hamzaharrass05@gmail.com <hamzaharrass05@gamil.com>
2022-09-11 17:32:29 +0100

    chellange

---
+++ semaine 1/fichier2/Untitled11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+ int main(){
+  	
+  	int i ;
+  	
+  	
+  	srand(time(NULL));
+  	
+  	i=rand()%12;
+  	
+  	switch (i)
+  	  case 1:printf("janv");
+  	 // break;
+  	   
+  	    printf("hamza");
+  	  
+  	return 0;  
+  }
--- semaine 1/fichier2/chellange10.c
+++ semaine 1/fichier2/chellange10.c
@@ -11,51 +11,59 @@
   	switch(mois){
 	  case 1:strcpy(Mois,"janv");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 2:strcpy(Mois,"fevr");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 3:strcpy(Mois,"mars");
 	  break;
-	  }
-	  switch(mois){
+	
+	  
 	  case 4:strcpy(Mois,"avr");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 5:strcpy(Mois,"mai");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 6:strcpy(Mois,"juin");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 7:strcpy(Mois,"juil");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 8:strcpy(Mois,"aout");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 9:strcpy(Mois,"sept");
 	  break;
-	  }
-	  switch(mois){
+	  
+	  
 	  case 10:strcpy(Mois,"oct");
 	  break;
-	  }
-	  switch(mois){
+	  
+
 	  case 11:strcpy(Mois,"nov");
 	  break;
-	  }
-	  switch(mois){
+
+
 	  case 12:strcpy(Mois,"dec");
 	  break;
-	  }
+	  
+	  
+	  default : printf("le mois n'existe pas'");
+	 
 	  printf("%d/%s/%d",jour,Mois,annee);
-  	
+	  
+	  }
+	  
+	 
+	  	 
+  	return 0;
   }
