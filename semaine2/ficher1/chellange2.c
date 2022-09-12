884e680504db4bc8cc110dfaa84a6ab5a865636e
hamzaharrass05@gmail.com <hamzaharrass05@gamil.com>
2022-09-12 01:06:29 +0100

    chellange

---
+++ semaine2/ficher1/chellange2.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+ int main(){
+ 	
+ 	int i;
+ 	
+ 	for(i=0;i<=15;i++)
+ 	{
+ 		switch(i){
+		 case 0:printf("\t *\n");
+ 			break ;
+ 		case 1:printf("\t***\n");
+ 			break ;
+ 		case 2:	printf("       ***** \n");
+ 			break;
+ 		case 3:printf("      *******\n");
+ 			break;
+ 		case 4:printf("     *********\n");
+ 			break;
+ 		case 5:printf("    ***********\n");
+ 			break;
+ 		case 6:printf("   *************\n");
+ 			break;
+ 		
+ 		}
+ 			
+	 }
+ }
