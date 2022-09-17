96a88215ecc4699fe7e7d667b7e9cf0697616ada
hamzaharrass05@gmail.com <hamzaharrass05@gamil.com>
2022-09-17 21:27:15 +0100

    chellange

--- projet sas/projet sas.c
+++ projet sas/projet sas.c
@@ -2,7 +2,8 @@
 #include<stdlib.h>
 #include<string.h>
 
-
+ 
+ 
 struct Produit {
     int code ;
     char nom[20];
@@ -10,32 +11,56 @@ struct Produit {
 	int quantite;	
 };
 
-      struct Produit P1;
-	  struct Produit P2;
-	  struct Produit P3;
+      struct Produit P[3];
+	  
+      int  length_array=0;
 
 
+    void Fadd(){
+ //	printf("ajouter un produit ");
+        int i,a;
+		printf("how much produit do you want add  ") 	;
+		scanf("%d",&a);
+ 	
+ 	    for(i=0;i<a;i++){
+ 	    	
+ 		   printf("\n entre le code de produit : \n");
+ 		   scanf("%d",&P[i].code);
+ 		
+ 		   printf("\n entre le nom de produit : \n");
+ 		   scanf("%s",&P[i].nom);
+ 		
+ 		   printf("\n entre le prix de produit : \n");
+ 		   scanf("%f",&P[i].prix);
+ 		
+ 		   printf("\n entre la quantite de produit : \n");
+ 		   scanf("%d",&P[i].quantite);
+ 		   
+ 		   length_array++;
+	  }
+	 }
+ 
 
- int Fadd(){
- 	printf("ajouter un produit ");
- }
+    int Frecherche(){
+ 	    printf("recherche un produit ");
+    }
 
-int Frecherche(){
- 	printf("recherche un produit ");
- }
-
-int Fsupprimer(){
- 	printf("suprimmer  un produit ");
- }
-
-void Fafficher(){
+    int Fsupprimer(){
+ 	    printf("suprimmer  un produit ");
+    }
+    	
+    void Fafficher(){
+    	int i,a;
+    	printf("    code   ||         nom           ||            prix         ||        quantite     ||\n");
+    	printf("_________________________________________________________________________________________________\n\n");
+	    for(i=0;i<length_array;i++){
+          printf("      %d    ||      %s         ||        %.2f dh         ||           %d       ||\n",P[i].code , P[i].nom , P[i].prix , P[i].quantite);
+     	}
  	//printf("afficher un produit ");
- 	printf("    code   ||         nom           ||            prix         ||        quantite     ||\n");
- 	printf("_________________________________________________________________________________________________\n\n");
-    printf("      %d    ||      %s         ||        %.2f dh         ||           %d       ||\n",P1.code , P1.nom , P1.prix , P1.quantite);
-    printf("      %d    ||      %s        ||         %.2f dh        ||            %d      ||\n",P2.code , P2.nom , P2.prix , P2.quantite);
-    printf("      %d    ||      %s         ||        %.2f dh         ||           %d       ||\n",P3.code , P3.nom , P3.prix , P3.quantite);
- }
+ 
+   // printf("      %d    ||      %s        ||         %.2f dh        ||            %d      ||\n",P2.code , P2.nom , P2.prix , P2.quantite);
+   // printf("      %d    ||      %s         ||        %.2f dh         ||           %d       ||\n",P3.code , P3.nom , P3.prix , P3.quantite);
+   }
 
 
  int main (){
@@ -45,33 +70,16 @@ void Fafficher(){
  	
  	int n;
   
-	   
-	  P1.code = 1 ;
-	  strcpy(P1.nom , "additiva");
-      P1.prix = 79 ;
-      P1.quantite = 150 ;
- 
-      P2.code = 2 ;
-      strcpy(P2.nom , "azimycine");
-      P2.prix = 66 ;
-      P2.quantite = 150 ;
-  
-  	  P3.code = 3 ;
-  	  strcpy(P3.nom , "dolipran");
-      P3.prix = 18 ;
-      P3.quantite = 150 ;
- 
- 
- 
+	
  	printf("-----------------------------------");
- 	printf("\n 1-add a product\n");
-    printf("\n 2-search for a product\n ");
- 	printf("\n 3-delete a product\n");
- 	printf("\n 4-display a product\n");
+ 	printf("\n  -> 1-add a product\n");
+    printf("\n  -> 2-search for a product\n ");
+ 	printf("\n  -> 3-delete a product\n");
+ 	printf("\n  -> 4-display a product\n");
  	printf("-----------------------------------");
- 		
+ 	
+ 	
  	     do{
- 	     	
  	     	printf ("\n choisir un choix  :  ");
  	     scanf("%d",&n);
  	     	
@@ -89,8 +97,10 @@ void Fafficher(){
  	     	case 4 : Fafficher();
  	     	break ;	
 	        }  
+	        
+	        
 		  
-		  }while(n<=4,n>0);
+		  }while(n<=4||n>0);
  	    
  	     
  return 0;	
